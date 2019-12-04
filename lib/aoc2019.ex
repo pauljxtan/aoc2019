275678a63832b4b06224b03c5612e2b12f715475
Paul Tan <pjxtan@gmail.com>
2019-12-03 20:46:46 -0500

    Add day 2, part 1

--- lib/aoc2019.ex
+++ lib/aoc2019.ex
@@ -5,23 +5,29 @@ defmodule Aoc2019 do
   def solutions,
     do: %{
       {1, 1} => Aoc2019.day1_part1_solve(),
-      {1, 2} => Aoc2019.day1_part2_solve()
+      {1, 2} => Aoc2019.day1_part2_solve(),
+      {2, 1} => Aoc2019.day2_part1_solve()
     }
 
   def day1_part1_solve(),
     do:
-      load_modules()
+      load_delim_input("inputs/input_day1", "\n")
       |> day1_part1()
 
   def day1_part2_solve(),
     do:
-      load_modules()
+      load_delim_input("inputs/input_day1", "\n")
       |> day1_part2()
 
-  def load_modules(),
+  def day2_part1_solve(),
     do:
-      File.read!("inputs/input_day1")
-      |> String.split("\n")
+      load_delim_input("inputs/input_day2", ",")
+      |> day2_part1()
+
+  def load_delim_input(filepath, delim),
+    do:
+      File.read!(filepath)
+      |> String.split(delim)
       |> Enum.map(&Integer.parse/1)
       |> Enum.reduce([], fn result, modules ->
         modules ++
@@ -30,6 +36,8 @@ defmodule Aoc2019 do
             :error -> []
           end
       end)
+
+  # -- Day 1
 
   def day1_part1(modules),
     do: modules |> Enum.reduce(0, fn mass, acc -> acc + fuel_req(mass) end)
@@ -42,5 +50,31 @@ defmodule Aoc2019 do
   defp fuel_req_recur(total_fuel, mass) do
     fuel = fuel_req(mass)
     if fuel > 0, do: fuel_req_recur(total_fuel + fuel, fuel), else: total_fuel
+  end
+
+  # -- Day 2
+
+  def day2_part1(program),
+    do: program |> List.replace_at(1, 12) |> List.replace_at(2, 2) |> eval_intcode() |> Enum.at(0)
+
+  def eval_intcode(program, idx \\ 0) do
+    opcode = program |> Enum.at(idx)
+
+    case opcode do
+      99 ->
+        program
+
+      _ ->
+        [i, j, k] = program |> Enum.slice(idx + 1, 3)
+        [x, y] = [i, j] |> Enum.map(fn k -> program |> Enum.at(k) end)
+
+        result =
+          case opcode do
+            1 -> x + y
+            2 -> x * y
+          end
+
+        program |> List.replace_at(k, result) |> eval_intcode(idx + 4)
+    end
   end
 end
--- test/aoc2019_test.exs
+++ test/aoc2019_test.exs
@@ -9,7 +9,7 @@ defmodule Aoc2019Test do
     assert day1_part1([1969]) == 654
     assert day1_part1([100_756]) == 33583
     assert day1_part1([12, 14, 1969, 100_756]) == 2 + 2 + 654 + 33583
-    assert day1_part1_solve() == 3405721
+    assert day1_part1_solve() == 3_405_721
   end
 
   test "day 1, part 2" do
@@ -17,6 +17,14 @@ defmodule Aoc2019Test do
     assert day1_part2([1969]) == 966
     assert day1_part2([100_756]) == 50346
     assert day1_part2([14, 1969, 100_756]) == 2 + 966 + 50346
-    assert day1_part2_solve() == 5105716
+    assert day1_part2_solve() == 5_105_716
+  end
+
+  test "day 2, part 1" do
+    assert eval_intcode([1, 0, 0, 0, 99]) == [2, 0, 0, 0, 99]
+    assert eval_intcode([2, 3, 0, 3, 99]) == [2, 3, 0, 6, 99]
+    assert eval_intcode([2, 4, 4, 5, 99, 0]) == [2, 4, 4, 5, 99, 9801]
+    assert eval_intcode([1, 1, 1, 4, 99, 5, 6, 0, 99]) == [30, 1, 1, 4, 2, 5, 6, 0, 99]
+    assert day2_part1_solve() == 4_138_658
   end
 end
